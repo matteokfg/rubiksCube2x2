d9efbca0e0a6164f6a8f91f6baa98f08bcabfe5a
matteokfg <67129306+matteokfg@users.noreply.github.com>
2026-05-06 22:56:16 -0300

    feat: criada movimentacao em quartetos
    As movimentacoes sao das duas camdas para cada eixo, no sentido horario e anti horario

--- rubiks_cube.py
+++ rubiks_cube.py
@@ -6,10 +6,22 @@ from OpenGL.GLU import *
 
 # Variáveis globais
 pos_x, pos_y, pos_z = 0, 0, -10
-angulo_rotacao_x = 45 # inicia o cubo inclinado, aparece o branco
-angulo_rotacao_y = -45 # inicia o cubo inclinado, aparece o vermelho
-angulo_rotacao_z = 0 # aparece o azul
-escala_obj = 1.0
+# angulo_rotacao_x = 45 # inicia o cubo inclinado, aparece o branco
+# angulo_rotacao_y = -45 # inicia o cubo inclinado, aparece o vermelho
+# angulo_rotacao_z = 0 # aparece o azul
+
+angulos = [
+    [45,-45,0],
+    [45,-45,0],
+    [45,-45,0],
+    [45,-45,0],
+    [45,-45,0],
+    [45,-45,0],
+    [45,-45,0],
+    [45,-45,0],
+]
+
+escala_obj = 0.1
 eixo_x = 0
 eixo_y = 0
 eixo_z = 0
@@ -391,6 +403,7 @@ def mini_cubo_8(): #amarelo,azul,vermelho
 
 def main():
     global pos_x, pos_y, pos_z, angulo_rotacao_x, angulo_rotacao_y, angulo_rotacao_z, escala_obj, eixo_x, eixo_y, eixo_z
+    global angulos
    
     pygame.init()
     display = (800, 600)
@@ -414,51 +427,149 @@ def main():
                 quit()
            
             if event.type == KEYDOWN:
-                if event.key == K_x: # rotaciona eixo x
-                    angulo_rotacao_x = 45 if angulo_rotacao_x >= 405 else angulo_rotacao_x + 90 # mantem o cubo inclinado para sempre ver 3 faces
-                    eixo_x = 1
-                    eixo_y = 0
-                    eixo_z = 0
-                if event.key == K_y: # rotaciona eixo y
-                    angulo_rotacao_y = 45 if angulo_rotacao_y > 405 else angulo_rotacao_y + 90 # mantem o cubo inclinado para sempre ver 3 faces
-                    eixo_x = 0
-                    eixo_y = 1
-                    eixo_z = 0
-                if event.key == K_z: # rotaciona eixo z
-                    angulo_rotacao_z = 0 if angulo_rotacao_z >= 360 else angulo_rotacao_z + 90 # mantem o cubo inclinado para sempre ver 3 faces
-                    eixo_x = 0
-                    eixo_y = 0
-                    eixo_z = 1
+                if event.key == K_q: # rotaciona eixo y sentido anti horario lado superior
+                    angulos[0][1] = -45 if angulos[0][1] >= 360 else angulos[0][1] + 90 
+                    angulos[1][1] = -45 if angulos[1][1] >= 360 else angulos[1][1] + 90 
+                    angulos[2][1] = -45 if angulos[2][1] >= 360 else angulos[2][1] + 90 
+                    angulos[3][1] = -45 if angulos[3][1] >= 360 else angulos[3][1] + 90
+                if event.key == K_p: # rotaciona eixo y sentido horario lado superior
+                    angulos[0][1] = -45 if angulos[0][1] <= -360 else angulos[0][1] - 90 
+                    angulos[1][1] = -45 if angulos[1][1] <= -360 else angulos[1][1] - 90 
+                    angulos[2][1] = -45 if angulos[2][1] <= -360 else angulos[2][1] - 90 
+                    angulos[3][1] = -45 if angulos[3][1] <= -360 else angulos[3][1] - 90
+                if event.key == K_s: # rotaciona eixo y sentido anti horario lado inferior
+                    angulos[4][1] = -45 if angulos[4][1] >= 360 else angulos[4][1] + 90 
+                    angulos[5][1] = -45 if angulos[5][1] >= 360 else angulos[5][1] + 90 
+                    angulos[6][1] = -45 if angulos[6][1] >= 360 else angulos[6][1] + 90 
+                    angulos[7][1] = -45 if angulos[7][1] >= 360 else angulos[7][1] + 90
+                if event.key == K_l: # rotaciona eixo y sentido anti horario lado inferior
+                    angulos[4][1] = -45 if angulos[4][1] <= -360 else angulos[4][1] - 90 
+                    angulos[5][1] = -45 if angulos[5][1] <= -360 else angulos[5][1] - 90 
+                    angulos[6][1] = -45 if angulos[6][1] <= -360 else angulos[6][1] - 90 
+                    angulos[7][1] = -45 if angulos[7][1] <= -360 else angulos[7][1] - 90
+
+
+
+                if event.key == K_w: # rotaciona eixo z sentido anti horario lado superior
+                    angulos[0][2] = 0 if angulos[0][2] >= 360 else angulos[0][2] + 90 
+                    angulos[1][2] = 0 if angulos[1][2] >= 360 else angulos[1][2] + 90 
+                    angulos[6][2] = 0 if angulos[6][2] >= 360 else angulos[6][2] + 90 
+                    angulos[7][2] = 0 if angulos[7][2] >= 360 else angulos[7][2] + 90
+                if event.key == K_o: # rotaciona eixo z sentido horario lado superior
+                    angulos[0][2] = 0 if angulos[0][2] <= -360 else angulos[0][2] - 90 
+                    angulos[1][2] = 0 if angulos[1][2] <= -360 else angulos[1][2] - 90 
+                    angulos[6][2] = 0 if angulos[6][2] <= -360 else angulos[6][2] - 90 
+                    angulos[7][2] = 0 if angulos[7][2] <= -360 else angulos[7][2] - 90
+                if event.key == K_d: # rotaciona eixo z sentido anti horario lado inferior
+                    angulos[2][2] = 0 if angulos[2][2] >= 360 else angulos[2][2] + 90 
+                    angulos[3][2] = 0 if angulos[3][2] >= 360 else angulos[3][2] + 90 
+                    angulos[4][2] = 0 if angulos[4][2] >= 360 else angulos[4][2] + 90 
+                    angulos[5][2] = 0 if angulos[5][2] >= 360 else angulos[5][2] + 90
+                if event.key == K_k: # rotaciona eixo z sentido anti horario lado inferior
+                    angulos[2][2] = 0 if angulos[2][2] <= -360 else angulos[2][2] - 90 
+                    angulos[3][2] = 0 if angulos[3][2] <= -360 else angulos[3][2] - 90 
+                    angulos[4][2] = 0 if angulos[4][2] <= -360 else angulos[4][2] - 90 
+                    angulos[5][2] = 0 if angulos[5][2] <= -360 else angulos[5][2] - 90
+
+
+
+                if event.key == K_e: # rotaciona eixo x sentido anti horario lado superior
+                    angulos[0][0] = 45 if angulos[0][0] >= 360 else angulos[0][0] + 90 
+                    angulos[3][0] = 45 if angulos[3][0] >= 360 else angulos[3][0] + 90 
+                    angulos[4][0] = 45 if angulos[4][0] >= 360 else angulos[4][0] + 90 
+                    angulos[7][0] = 45 if angulos[7][0] >= 360 else angulos[7][0] + 90
+                if event.key == K_i: # rotaciona eixo x sentido horario lado superior
+                    angulos[0][0] = 45 if angulos[0][0] <= -360 else angulos[0][0] - 90 
+                    angulos[3][0] = 45 if angulos[3][0] <= -360 else angulos[3][0] - 90 
+                    angulos[4][0] = 45 if angulos[4][0] <= -360 else angulos[4][0] - 90 
+                    angulos[7][0] = 45 if angulos[7][0] <= -360 else angulos[7][0] - 90
+                if event.key == K_f: # rotaciona eixo x sentido anti horario lado inferior
+                    angulos[1][0] = 45 if angulos[1][0] >= 360 else angulos[1][0] + 90 
+                    angulos[2][0] = 45 if angulos[2][0] >= 360 else angulos[2][0] + 90 
+                    angulos[5][0] = 45 if angulos[5][0] >= 360 else angulos[5][0] + 90 
+                    angulos[6][0] = 45 if angulos[6][0] >= 360 else angulos[6][0] + 90
+                if event.key == K_j: # rotaciona eixo x sentido anti horario lado inferior
+                    angulos[1][0] = 45 if angulos[1][0] <= -360 else angulos[1][0] - 90 
+                    angulos[2][0] = 45 if angulos[2][0] <= -360 else angulos[2][0] - 90 
+                    angulos[5][0] = 45 if angulos[5][0] <= -360 else angulos[5][0] - 90 
+                    angulos[6][0] = 45 if angulos[6][0] <= -360 else angulos[6][0] - 90
+                
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 4: # scroll up
                     escala_obj += 0.1 # aumenta zoom
                 if event.button == 5: # scrool down
                     escala_obj -= 0.1 # diminui zoom
-       
+        
         # Fundo preto
         glClearColor(0, 0, 0, 1.0)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
        
         glLoadIdentity()
-        
-        #glPushMatrix()
-        glTranslatef(pos_x, pos_y, pos_z)
 
-        glRotatef(angulo_rotacao_x, 1, 0, 0)
-        glRotatef(angulo_rotacao_y, 0, 1, 0)
-        glRotatef(angulo_rotacao_z, 0, 0, 1)
-        
-        glScalef(escala_obj, escala_obj, escala_obj)
-        mini_cubo_8()
-        mini_cubo_7()
-        mini_cubo_6()
-        mini_cubo_5()
-        mini_cubo_4()
-        mini_cubo_3()
-        mini_cubo_2()
+        glPushMatrix()
+        glTranslatef(pos_x, pos_y, pos_z)
+        glRotatef(angulos[0][0], 1, 0, 0)
+        glRotatef(angulos[0][1], 0, 1, 0)
+        glRotatef(angulos[0][2], 0, 0, 1)
         mini_cubo_1()
-        #glPopMatrix()
+        glPopMatrix()
+
+        glPushMatrix()
+        glTranslatef(pos_x, pos_y, pos_z)
+        glRotatef(angulos[1][0], 1, 0, 0)
+        glRotatef(angulos[1][1], 0, 1, 0)
+        glRotatef(angulos[1][2], 0, 0, 1)
+        mini_cubo_2()
+        glPopMatrix()
+
+        glPushMatrix()
+        glTranslatef(pos_x, pos_y, pos_z)
+        glRotatef(angulos[2][0], 1, 0, 0)
+        glRotatef(angulos[2][1], 0, 1, 0)
+        glRotatef(angulos[2][2], 0, 0, 1)
+        mini_cubo_3()
+        glPopMatrix()
+
+        glPushMatrix()
+        glTranslatef(pos_x, pos_y, pos_z)
+        glRotatef(angulos[3][0], 1, 0, 0)
+        glRotatef(angulos[3][1], 0, 1, 0)
+        glRotatef(angulos[3][2], 0, 0, 1)
+        mini_cubo_4()
+        glPopMatrix()
+
+        glPushMatrix()
+        glTranslatef(pos_x, pos_y, pos_z)
+        glRotatef(angulos[4][0], 1, 0, 0)
+        glRotatef(angulos[4][1], 0, 1, 0)
+        glRotatef(angulos[4][2], 0, 0, 1)
+        mini_cubo_5()
+        glPopMatrix()
+
+        glPushMatrix()
+        glTranslatef(pos_x, pos_y, pos_z)
+        glRotatef(angulos[5][0], 1, 0, 0)
+        glRotatef(angulos[5][1], 0, 1, 0)
+        glRotatef(angulos[5][2], 0, 0, 1)
+        mini_cubo_6()
+        glPopMatrix()
+
+        glPushMatrix()
+        glTranslatef(pos_x, pos_y, pos_z)
+        glRotatef(angulos[6][0], 1, 0, 0)
+        glRotatef(angulos[6][1], 0, 1, 0)
+        glRotatef(angulos[6][2], 0, 0, 1)
+        mini_cubo_7()
+        glPopMatrix()
+
+        glPushMatrix()
+        glTranslatef(pos_x, pos_y, pos_z)
+        glRotatef(angulos[7][0], 1, 0, 0)
+        glRotatef(angulos[7][1], 0, 1, 0)
+        glRotatef(angulos[7][2], 0, 0, 1)
+        mini_cubo_8()
+        glPopMatrix()
 
         pygame.display.flip()
         pygame.time.wait(10)
